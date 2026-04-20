b3e64c010477f788c8a438853f519e61b4b7da54
KrishBawangade <krishbawangade08@gmail.com>
2026-04-20 23:48:47 +0530

    chore: add core dependencies and env setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_high_performance_feed/core/env/env.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: Env.supabaseUrl,
+    anonKey: Env.supabaseAnonKey,
+  );
+  
   runApp(const MainApp());
 }
 
